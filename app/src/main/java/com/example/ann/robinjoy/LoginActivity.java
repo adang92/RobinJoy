9ff4cad4020fb2f1fe73add0e180d808149a4939
Ann <ann.uyen.dang@gmail.com>
2017-03-22 20:32:38 -0400

    For arsalans amusment

--- app/src/main/java/com/example/ann/robinjoy/LoginActivity.java
+++ app/src/main/java/com/example/ann/robinjoy/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.ann.robinjoy;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -346,7 +347,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                //finish();
+                //Go to Homepage
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
--- app/src/main/java/com/example/ann/robinjoy/Requests.java
+++ app/src/main/java/com/example/ann/robinjoy/Requests.java
@@ -1,13 +1,55 @@
 package com.example.ann.robinjoy;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class Requests extends AppCompatActivity {
+public class Requests extends Activity implements OnItemSelectedListener{
+
+    Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+    Spinner spinner2 = (Spinner)findViewById(R.id.spinner2);
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requests);
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        spinner1.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                               long arg3) {
+        // TODO Auto-generated method stub
+        String sp1 = String.valueOf(spinner1.getSelectedItem());
+        Toast.makeText(this, sp1, Toast.LENGTH_SHORT).show();
+
+        if (sp1.contentEquals("Hygiene")) {
+            adapter = ArrayAdapter.createFromResource(this, R.array.Hygiene, android.R.layout.simple_spinner_dropdown_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            spinner2.setAdapter(adapter);
+        }
+
+        if (sp1.contentEquals("Food")) {
+            adapter = ArrayAdapter.createFromResource(this, R.array.Food, android.R.layout.simple_spinner_dropdown_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            spinner2.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
+
+
+
 }
